126417df963e8b3dd40caa5cbdac3305b70d1edd
jacopodaffy-cloud <jacopodaffy@gmail.com>
2026-06-15 21:26:25 +0200

    Fix webDir and copy web assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.quicklist.twa',
   appName: 'QuickList',
-  webDir: '.',
+  webDir: 'www',
 };
 
 export default config;
