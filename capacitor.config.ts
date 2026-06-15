b1349b0412584d7be195621320f6cc93ec592a90
jacopodaffy-cloud <jacopodaffy@gmail.com>
2026-06-15 21:20:34 +0200

    Add Capacitor + GitHub Actions

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.quicklist.twa',
+  appName: 'QuickList',
+  webDir: '.',
+};
+
+export default config;
