54168c7649a8b995bf7021eefc9bf80a510f43ce
jacopodaffy-cloud <jacopodaffy@gmail.com>
2026-06-16 22:24:54 +0200

    Load app from Firebase Hosting URL

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'app.quicklist.twa',
   appName: 'QuickList',
   webDir: 'www',
+  server: {
+    url: 'https://qwicklist-v3.web.app',
+    cleartext: false
+  }
 };
 
 export default config;
